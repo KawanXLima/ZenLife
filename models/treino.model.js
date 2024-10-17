afe527532130d4769cb44e5152092d650cef7a8e
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-10-17 15:03:26 -0300

    foi criado a configuração de conexao com o bd e o model treino

---
+++ models/treino.model.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Treino = sequelize.define("treino", {
+    horario_inicial: {
+      type: DataTypes.TIME(6),
+    },
+    horario_final: {
+      type: DataTypes.TIME(6),
+    },
+    duracao: {
+      type: DataTypes.TIME(6),
+    },
+    dia_atual: {
+      type: DataTypes.DATE,
+    },
+  });
+
+  return Treino;
+};
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
+const db = require("./models/index");
 
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+db.sequelize.sync().then(()=>{
+  console.log("Synce db");
+}).catch((err)=>{
+  console.log("Failed to sync db: " + err.message);
+})
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to ZenLife API" });
