324fca04b5cd221b095d876c4a7ba07b696dc2a0
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-10-20 19:02:20 -0300

    adicionado nova rota para geração da rotina

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-db.sequelize.sync().then(()=>{
-  console.log("Synce db");
-}).catch((err)=>{
-  console.log("Failed to sync db: " + err.message);
-})
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synce db");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
@@ -28,6 +31,8 @@ db.sequelize.sync({ force: true }).then(() => {
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to ZenLife API" });
 });
+
+require("./routes/rotina.route")(app);
 
 const PORT = process.env.NODE_LOCAL_PORT || 8080;
 
