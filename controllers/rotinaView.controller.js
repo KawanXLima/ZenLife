8591ed6343f85ad796670b946bb2baa03588a388
Marley de Almeida <almeida.marley456@gmail.com>
2024-10-27 22:53:42 -0300

    criação do método de devolução dos dados

---
+++ controllers/rotinaView.controller.js
@@ -0,0 +1,25 @@
+const db = require("../models/index");
+const Rotina = db.rotina;
+const Treino = db.treino;
+
+
+exports.findAll = async (req, res) => {
+    // Find all users
+    try {
+        const rotina = await Rotina.findAll({
+            include: [{
+                model: Treino,
+                as: "treinos", // Certifique-se de que este alias está correto
+            }],
+        });
+
+        res.send(rotina);
+    }
+    catch (error) {
+        res.status(404).send({
+            message:
+                error.message || "Some error occurred while retrieving funcionario.",
+        });
+
+    }
+};
