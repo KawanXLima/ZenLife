ef0cf329a9d6061f600dcad3359a147a5267ae01
Marley de Almeida <almeida.marley456@gmail.com>
2024-10-27 22:56:14 -0300

    criação da rota de busca dos dados enviados(findAll)

--- routes/rotina.route.js
+++ routes/rotina.route.js
@@ -1,10 +1,10 @@
 module.exports = (app) => {
   const rotina = require("../controllers/rotina.controller.js");
-
+  const rotinaView = require("../controllers/rotinaView.controller.js");
   var router = require("express").Router();
 
   // Create a new Rotina
   router.post("/", rotina.create);
-  
+  router.get("/", rotinaView.findAll);
   app.use("/api/rotina", router);
 };
