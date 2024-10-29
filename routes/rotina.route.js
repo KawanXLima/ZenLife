a525b5bb611ab865af086b829065d8ef2c6a012d
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-10-29 14:07:46 -0300

    alteração do nome das variáveis e importações de arquivo

--- routes/rotina.route.js
+++ routes/rotina.route.js
@@ -1,10 +1,10 @@
 module.exports = (app) => {
-  const rotina = require("../controllers/rotina.controller.js");
-  const rotinaView = require("../controllers/rotinaView.controller.js");
+  const rotinaGerar = require("../controllers/rotina-controller/rotinaGerar.controller");
+  const rotinaView = require("../controllers/rotina-controller/rotinaView.controller");
   var router = require("express").Router();
 
   // Create a new Rotina
-  router.post("/", rotina.create);
+  router.post("/", rotinaGerar.create);
   router.get("/", rotinaView.findAll);
   app.use("/api/rotina", router);
 };
