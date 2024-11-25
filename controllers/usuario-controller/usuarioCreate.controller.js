cfcc1aaa6fde1110b57496438bff20ed81ab6ed0
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-11-25 11:16:09 -0300

    Foi feito o código para criptografar e validar o registro de usuario

--- controllers/usuario-controller/usuarioCreate.controller.js
+++ controllers/usuario-controller/usuarioCreate.controller.js
@@ -1,33 +1,43 @@
+const bcrypt = require("bcrypt");
 const db = require("../../models/index");
 const Usuario = db.usuario;
 
 exports.create = async (req, res) => {
-  const { nome, login, senha, data_nasc, genero } = req.body;
+  const { nome, login, senha, senha2, data_nasc, genero } = req.body;
 
-  if (!nome || !login || !senha || !data_nasc || !genero) {
-    return res.status(400).send({
-      message: "Content can not be empty! Please fill all the fields.",
-    });
+  if (!nome || !login || !senha || !senha2 || !data_nasc || !genero) {
+    return res
+      .status(400)
+      .json({ message: "Todos os campos são obrigatórios." });
   }
-  const usuario = {
-    nome: nome,
-    login: login,
-    senha: senha,
-    data_nasc: data_nasc,
-    genero: genero,
-  };
+
+  if (senha !== senha2) {
+    return res.status(400).json({ message: "As senhas não coincidem." });
+  }
 
   try {
-    const result = await Usuario.create(usuario);
-    if (!result) {
-      return res.status(400).send({
-        message: "Content can not be empty! Please fill all the fields.",
-      });
+    const usuarioExistente = await Usuario.findOne({ where: { login } });
+    if (usuarioExistente) {
+      return res.status(400).json({ message: "Usuário já existe." });
     }
-    return res.send({ data: true });
+
+    const senhaHash = await bcrypt.hash(senha, 10);
+
+    const novoUsuario = await Usuario.create({
+      nome,
+      login,
+      senha: senhaHash,
+      data_nasc,
+      genero,
+    });
+
+    return res
+      .status(201)
+      .json({ message: "Usuário criado com sucesso.", usuario: novoUsuario });
   } catch (error) {
-    res.status(500).send({
-      message: "An error occurred while generating the usuario.",
+    console.error(error);
+    return res.status(500).json({
+      message: "Ocorreu um erro ao processar a solicitação.",
       error: error.message,
     });
   }
